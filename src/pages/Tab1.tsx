7cd18631eaf356a6fc192717af8feac8852c1abf
Mijail <mijail.devops@gmail.com>
2020-08-09 17:03:45 -0500

    Click en mapa y en Maker

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -13,6 +13,39 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
   return marker;
  };
 
+ function initMap(): void {
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+ // @ts-ignore
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 4,
+      center: myLatlng
+    }
+  );
+ // @ts-ignore
+  const marker = new google.maps.Marker({
+    position: myLatlng,
+    map,
+    title: "Click to zoom"
+  });
+
+  map.addListener("center_changed", () => {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(() => {
+       // @ts-ignore
+      map.panTo(marker.getPosition() as google.maps.LatLng);
+    }, 3000);
+  });
+
+  marker.addListener("click", () => {
+    map.setZoom(8);
+     // @ts-ignore
+    map.setCenter(marker.getPosition() as google.maps.LatLng);
+  });
+}
+
  
 class Tab1 extends Component {
   static defaultProps = {
@@ -21,7 +54,9 @@ class Tab1 extends Component {
       lng: 30.33
     },
     zoom: 15
+    
   };
+
  
   render() {
     return (
@@ -33,12 +68,15 @@ class Tab1 extends Component {
           defaultCenter={this.props.center}
           //@ts-ignore
           defaultZoom={this.props.zoom}
+          // @ts-ignore
+         
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps, 
             // @ts-ignore
             59.95,
             // @ts-ignore 
             30.33)}
+            
         >
           <AnyReactComponent
           //@ts-ignore
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,26 +1,116 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ //@ts-ignore
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+ // @ts-ignore
+ const renderMarkers = (map, maps, Lat, Long) => {
+  let marker = new maps.Marker({
+  position: { lat: Lat, lng: Long },
+  map,
+  title: 'Hello World!'
+  });
+  return marker;
+ };
+
+
+
+
+ 
+class Tab2 extends Component {
+  /* static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 15
+    
+  }; */
+
+  state = {
+    center: [60.938043, 30.337157],
+    zoom: 17,
+    draggable: true,
+    lat: 60.955413,
+    lng: 30.337844
+  };
+  //@ts-ignore
+  onCircleInteraction(childKey, childProps, mouse) {
+    // function is just a stub to test callbacks
+    this.setState({
+      draggable: true,
+      lat: mouse.lat,
+      lng: mouse.lng
+    });
+   
+    console.log('onCircleInteraction called with', childKey, childProps, mouse);
+  }
+  //@ts-ignore
+  onCircleInteraction3(childKey, childProps, mouse) {
+    this.setState({draggable: true});
+    // function is just a stub to test callbacks  
+    console.log('onCircleInteraction called with', childKey, childProps, mouse);
+    
+  }
+  //@ts-ignore
+    _onChange = ({center, zoom}) => {
+      this.setState({
+        center: center,
+        zoom: zoom,      
+      });
+    }
+
+
+
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        
+        <GoogleMapReact 
+        //@ts-ignore
+        draggable={this.state.draggable}
+        //@ts-ignore
+        onChange={this._onChange}
+        //@ts-ignore
+        center={this.state.center}
+        //@ts-ignore
+        zoom={this.state.zoom}
+        //@ts-ignore
+        onChildMouseDown={this.onCircleInteraction}
+        //@ts-ignore
+        onChildMouseUp={this.onCircleInteraction3}
+        //@ts-ignore
+        onChildMouseMove={this.onCircleInteraction}    
+        //@ts-ignore
+        onChildClick={() => console.log('child click')}
+        //@ts-ignore
+        onClick={() => console.log('mapClick')}
+        yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps, 
+            // @ts-ignore
+            60.955413,
+            // @ts-ignore 
+            30.337844)
+            
+          }
+        
+       >
+        <div 
+          className="place" 
+          //@ts-ignore
+          lat={this.state.lat} 
+          //@ts-ignore
+          lng={this.state.lng}>
+            12
+       </div>
+      </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+/* export default SimpleMap; */
 
 export default Tab2;
