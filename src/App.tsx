9cd0db9c7d8071b74f3a0de83acaa5984c18d8ac
Mijail-Devops <mijail.devops@gmail.com>
2020-08-09 19:47:47 -0500

    log

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
-import GoogleMapReact from 'google-map-react';
 
 
 /* Core CSS required for Ionic components to work properly */
